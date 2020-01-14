59f3259be9a846313ee5ecf1196730c9ef87be47
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-14 15:50:17 +0100

    Aggiunto bonus
    Aggiunta difficoltà inizale

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 // Gioco Campo minato
 // -----------------------------
 
+
+// Bonus, scegli la difficoltà
+var difficolta = prompt('Scegli la difficoltà. Scrivi 0, 1 o 2');
+
+if (difficolta == 0) {
+  var numeriMax = 100;
+  var tentativi = 84;
+} else if (difficolta == 1) {
+  numeriMax = 80;
+  tentativi = 64;
+} else if (difficolta == 2) {
+  numeriMax = 50;
+  tentativi = 34;
+}
+
+
 // Il computer deve generare 16 numeri casuali da 1 a 100.
 var numeriVietati = [];
 
 for (var i = 0; i < 16; i++) {
-  numeriVietati.push(getRandomInt(1, 100));
+  numeriVietati.push(getRandomInt(1, numeriMax));
 }
 console.log(numeriVietati);
 
@@ -15,7 +31,7 @@ var numeroInserito,
     j = 0,
     punteggio = 0;
 
-while (j < 10) {
+while (j < tentativi) {
   numeroInserito = prompt('Inserisci un numero sempre diverso da 1 a 100')
   console.log('Numero inserito ' + (j + 1) + ': ' + numeroInserito);
 
@@ -26,7 +42,7 @@ while (j < 10) {
       // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di tentativi consentiti.
       // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
       alert('Hai inserito un numero "vietato", il tuo punteggio è ' + punteggio);
-      j = 10;
+      j = tentativi;
     }
   }
   j++;
